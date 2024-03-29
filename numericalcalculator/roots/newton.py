4601f3619457ae489bc3d24c917daafbb4656a68
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-29 12:32:51 -0400

    Add newton method

--- numericalcalculator/roots/newton.py
+++ numericalcalculator/roots/newton.py
@@ -4,3 +4,19 @@
     method with bisection. The root must be bracketed in (a,b).
     Calls user-supplied functions f(x) and its derivative df(x).
 """
+import numpy as np
+
+
+def newton(f, df, x, tol = 1.0e-5, max_iter = 150):
+    xn = x
+    for _ in range(0, max_iter):
+        fxn = f(xn)
+        if np.abs(fxn) < tol:
+            return xn
+
+        dfxn = df(xn)
+        if dfxn == 0:
+            raise Exception("La derivada da igual a 0")
+
+        xn = xn - fxn/dfxn
+    return xn
