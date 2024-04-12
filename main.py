c56d5208a3b5e2615609e72b3360b02b034883d0
Andres Alvarez <andres15alvarez@gmail.com>
2024-04-12 14:39:41 -0400

    Add calculations in views

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+from methods.functions import df2b, df2c, f2b, f2c, get_f2b_range, get_f2c_range
+from methods.roots.newton import newton
+from methods.roots.secant import secant
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -12,18 +16,74 @@ templates = Jinja2Templates(directory="templates")
 def index(request: Request):
     return templates.TemplateResponse(request, "index.html", { "greeting": "Hello World" })
 
-@app.get("/raices", response_class=HTMLResponse)
-def raices(request: Request):
-    return templates.TemplateResponse(request, "raices.html", { "greeting": "Hello World" })
 
-@app.get("/sistemas", response_class=HTMLResponse)
-def sistemas(request: Request):
-    return templates.TemplateResponse(request, "sistemas.html", { "greeting": "Hello World" })
+@app.get("/roots", response_class=HTMLResponse)
+def roots(request: Request):
+    return templates.TemplateResponse(request, "roots.html", { "greeting": "Hello World" })
 
-@app.get("/raices-resolucion", response_class=HTMLResponse)
-def sistemas(request: Request):
-    return templates.TemplateResponse(request, "raices-resolucion.html", { "greeting": "Hello World" })
 
-@app.get("/sistemas-resolucion", response_class=HTMLResponse)
-def sistemas(request: Request):
-    return templates.TemplateResponse(request, "sistemas-resolucion.html", { "greeting": "Hello World" })
+@app.get("/equationssystem", response_class=HTMLResponse)
+def equationssystem(request: Request):
+    return templates.TemplateResponse(request, "equationssystem.html", { "greeting": "Hello World" })
+
+
+@app.get("/roots/1b", response_class=HTMLResponse)
+def bisection1b(request: Request):
+    return templates.TemplateResponse(request, "roots_solution.html", { "greeting": "Hello World" })
+
+
+@app.get("/roots/1c", response_class=HTMLResponse)
+def bisection1c(request: Request):
+    return templates.TemplateResponse(request, "roots_solution.html", { "greeting": "Hello World" })
+
+
+@app.get("/roots/2b/newton", response_class=HTMLResponse)
+def newton2b(request: Request):
+    results = []
+    value = newton(f2b, df2b, 4, results)
+    f_value = f2b(value)
+    print(len(results))
+    return templates.TemplateResponse(
+        request, "roots_solution.html", { "x": value, "fx": f_value, "results": results }
+    )
+
+
+@app.get("/roots/2c/newton", response_class=HTMLResponse)
+def newton2c(request: Request):
+    results = []
+    value = newton(f2c, df2c, 1.6, results)
+    f_value = f2c(value)
+    return templates.TemplateResponse(
+        request, "roots_solution.html", { "x": value, "fx": f_value, "results": results }
+    )
+
+
+@app.get("/roots/2b/secant", response_class=HTMLResponse)
+def secant2b(request: Request):
+    results = []
+    a, b = get_f2b_range()
+    value = secant(f2b, a, b, results)
+    f_value = f2b(value)
+    return templates.TemplateResponse(
+        request, "roots_solution.html", { "x": value, "fx": f_value, "results": results }
+    )
+
+
+@app.get("/roots/2c/secant", response_class=HTMLResponse)
+def secant2c(request: Request):
+    results = []
+    a, b = get_f2c_range()
+    value = secant(f2c, a, b, results)
+    f_value = f2b(value)
+    return templates.TemplateResponse(
+        request, "roots_solution.html", { "x": value, "fx": f_value, "results": results }
+    )
+
+
+@app.get("/equationssystem/1", response_class=HTMLResponse)
+def equationssystem(request: Request):
+    return templates.TemplateResponse(request, "equationssystem_solution.html", { "greeting": "Hello World" })
+
+@app.get("/equationssystem/2", response_class=HTMLResponse)
+def equationssystem(request: Request):
+    return templates.TemplateResponse(request, "equationssystem_solution.html", { "greeting": "Hello World" })
