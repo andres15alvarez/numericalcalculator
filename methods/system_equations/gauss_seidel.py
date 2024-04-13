7166da94749e3d9ad4de98ba849453d9f43fa1a9
Andres Alvarez <andres15alvarez@gmail.com>
2024-04-13 15:40:10 -0400

    Add number of iteration and fix condition

--- methods/system_equations/gauss_seidel.py
+++ methods/system_equations/gauss_seidel.py
@@ -4,13 +4,15 @@ import numpy as np
 def gauss_seidel(A, b, tol = 1e-5, max_iterations = 150):
     x = np.zeros_like(b, dtype=np.double)
     e = x.copy()
+    counter = 0
 
     for _ in range(max_iterations):
+        counter += 1
         x_old  = x.copy()
 
         for i in range(A.shape[0]):
             x[i] = (b[i] - np.dot(A[i,:i], x[:i]) - np.dot(A[i,(i+1):], x_old[(i+1):])) / A[i,i]
             e[i] = np.abs((x[i] - x_old[i]) / x[i])
-        if e.all() < tol:
-            return x
-    return x
+        if (e < tol).all():
+            return x, counter
+    return x, counter
